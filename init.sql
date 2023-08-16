5db45bc5370c55960e1d31d8aa4d6beb3a295841
Fresh-Instinct <Paulakpata358@gmail.com>
2023-08-17 00:02:31 +0100

    my java app using docker compose

---
+++ init.sql
@@ -0,0 +1,10 @@
+-- init.sql
+CREATE TABLE Employee (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(250),
+  last_name VARCHAR(250),
+  email VARCHAR(250),
+  username VARCHAR(250),
+  password VARCHAR(2500,
+  regdate timestamp
+);
